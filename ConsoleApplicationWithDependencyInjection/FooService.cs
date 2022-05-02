db721ab4eb5fcf1ddd2f5fdb09cb64021f55ff00
emad sayed <emad_tttt52@hotmail.com>
2022-05-02 14:40:09 +0200

    init

---
+++ ConsoleApplicationWithDependencyInjection/FooService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationWithDependencyInjection
+{
+    public class FooService : IFooService
+    {
+        public FooService(IConfiguration _configuration)
+        {
+            var we = _configuration.GetValue<string>("Op1");
+        }
+        public void GetFoo()
+        {
+            Console.WriteLine("GetFoo Called !!");
+        }
+    }
+}
---
+++ ConsoleApplicationWithDependencyInjection/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApplicationWithDependencyInjection
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            var ServiceProvider = host.Services;
+            ServiceProvider.GetService<IFooService>().GetFoo();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, builder) =>
+                {
+                    builder.AddJsonFile("appsettings.json");
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<IFooService, FooService>();
+                });
+            return hostBuilder;
+        }
+    }
+}
